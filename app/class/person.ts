4b8be4521eb2f0ab61a25d2034beba2d9f2505dc
Kai Garrott <garrottkai@gmail.com>
2017-01-23 22:01:23 -0700

    create files

---
+++ app/class/person.ts
@@ -0,0 +1,9 @@
+export class Person {
+  constructor(
+    public personId: number,
+    public personName: string,
+    public personTitle: string,
+    public personAssets: number,
+    public personDescription: string
+  )
+};
---
+++ app/component/home-component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+import {Router} from "@angular/router";
+
+@Component({
+	templateUrl: "./template/home-template.html"
+
+});
+
+export class HomeComponent {
+
+};
